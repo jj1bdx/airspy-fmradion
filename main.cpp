ac539abd623a4516f771c9c605d40865235ea8fb
Kenji Rikitake <kenji.rikitake@acm.org>
2019-01-18 12:07:10 +0900

    Version 0.1.6

--- main.cpp
+++ main.cpp
@@ -44,7 +44,7 @@
 
 #include "EqParameters.h"
 
-#define NGSOFTFM_VERSION "0.1.5"
+#define NGSOFTFM_VERSION "0.1.6"
 
 /** Flag is set on SIGINT / SIGTERM. */
 static std::atomic_bool stop_flag(false);
