eb5a8b05e76c9d153e95fd0e433ee2eae444fe07
Kenji Rikitake <kenji.rikitake@acm.org>
2019-01-30 09:58:02 +0900

    (Unofficial) Version 0.1.13-dev, not tagged

--- main.cpp
+++ main.cpp
@@ -43,7 +43,7 @@
 #include "HackRFSource.h"
 #include "RtlSdrSource.h"
 
-#define NGSOFTFM_VERSION "0.1.12"
+#define NGSOFTFM_VERSION "0.1.13-dev"
 
 /** Flag is set on SIGINT / SIGTERM. */
 static std::atomic_bool stop_flag(false);
