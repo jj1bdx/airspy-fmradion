611796aa793d64da87ba8df4c49378f2b305e02b
Kenji Rikitake <kenji.rikitake@acm.org>
2019-03-02 18:28:23 +0900

    Add doc/fmradion*.py

---
+++ doc/fmradion.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# Simple caller for softfm
+import sys
+import subprocess
+import signal
+
+argvs = sys.argv
+argc = len(argvs)
+
+def signal_handler(signal, frame):
+    print('Terminated by CTRL/C')
+    sys.exit(0)
+
+signal.signal(signal.SIGINT, signal_handler)
+
+if argc != 2:
+    print('Usage: ', argvs[0], '<frequency in MHz>\n')
+    quit()
+freq = int(float(argvs[1]) * 1000000)
+command = "airspy-fmradion -q -c freq=" + str(freq) + \
+           ",srate=10000000,lgain=3,mgain=0,vgain=10 -r 44100 -b 1.0 -R - | " + \
+          "play -t raw -esigned-integer -b16 -r 44100 -c 2 -q -"
+print("command =", command)
+subprocess.run(command, shell=True, check=True)
