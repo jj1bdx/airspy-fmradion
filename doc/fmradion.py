3542b5dab600d64f93a940c9d85f79aa95263173
Kenji Rikitake <kenji.rikitake@acm.org>
2019-03-04 18:54:40 +0900

    doc/fmradion.py: change audio rate to 48kHz

--- doc/fmradion.py
+++ doc/fmradion.py
@@ -18,7 +18,7 @@ if argc != 2:
     quit()
 freq = int(float(argvs[1]) * 1000000)
 command = "airspy-fmradion -q -c freq=" + str(freq) + \
-           ",srate=10000000,lgain=3,mgain=0,vgain=10 -r 44100 -b 1.0 -R - | " + \
-          "play -t raw -esigned-integer -b16 -r 44100 -c 2 -q -"
+           ",srate=10000000,lgain=3,mgain=0,vgain=10 -r 48000 -b 1.0 -R - | " + \
+          "play -t raw -esigned-integer -b16 -r 48000 -c 2 -q -"
 print("command =", command)
 subprocess.run(command, shell=True, check=True)
