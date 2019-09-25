a8b7c30b974f2059ba01ca0805abaecd837b7ebe
Kenji Rikitake <kenji.rikitake@acm.org>
2019-09-25 10:07:19 +0900

    Version v0.7.8-pre1

--- main.cpp
+++ main.cpp
@@ -48,7 +48,7 @@
 // define this for enabling coefficient monitor functions
 // #undef COEFF_MONITOR
 
-#define AIRSPY_FMRADION_VERSION "v0.7.8-pre0-namespace"
+#define AIRSPY_FMRADION_VERSION "v0.7.8-pre1"
 
 /** Flag is set on SIGINT / SIGTERM. */
 static std::atomic_bool stop_flag(false);
