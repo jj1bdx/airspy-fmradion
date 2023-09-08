29370f82176801ef44e27d4d7a7a54301c59032e
Kenji Rikitake <kenji.rikitake@acm.org>
2023-09-08 12:56:20 +0900

    Version 20230908-0-test, not released

--- main.cpp
+++ main.cpp
@@ -54,7 +54,7 @@
 // define this for monitoring DataBuffer queue status
 // #undef DATABUFFER_QUEUE_MONITOR
 
-#define AIRSPY_FMRADION_VERSION "20230728-0-test"
+#define AIRSPY_FMRADION_VERSION "20230908-0-test"
 
 // Flag to set graceful termination
 // in process_signals()
