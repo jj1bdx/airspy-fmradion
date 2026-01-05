c0fe279b124855af34c195c72109eac3945a184d
Kenji Rikitake <kenji.rikitake@acm.org>
2026-01-05 21:15:23 +0900

    fm-pll-filtereval.py: use more practical values

--- doc/fm-pll-filtereval.py
+++ doc/fm-pll-filtereval.py
@@ -3,16 +3,18 @@ import scipy.signal as signal
 import numpy as np
 import matplotlib.pyplot as plt
 
-# simulated integrator
-sosinteg = [1e-6, 0, 0, 1, -0.999999, 0]
+# pure integrator with fixed gain of 80dB (10000)
+sosinteg = [10000.0, 0.0, 0.0, 1.0, -1.0, 0.0]
 # 2nd-order LPF
 sosprelpf = [1.46974784e-06, 0, 0, 1, -1.99682419, 0.996825659]
 # 1st-order HPF (or inversed LPF)
 sosfirsthpf = [0.000304341788, -0.000304324564, 0, 1, 0, 0]
 
-sostest = [sosprelpf, sosprelpf, sosfirsthpf, sosinteg, sosinteg]
+sostest = [sosprelpf, sosprelpf, sosfirsthpf, sosfirsthpf, sosinteg]
 
-sosref = [sosprelpf, sosfirsthpf, sosinteg, sosinteg]
+sosref = [sosprelpf, sosfirsthpf, sosinteg]
+
+soshpfonly = [sosfirsthpf]
 
 sostwofilt = [sosprelpf, sosfirsthpf]
 
@@ -21,7 +23,7 @@ w, h = signal.sosfreqz(sosref, 38400000, False, 384000)
 plt.subplot(2, 1, 1)
 db = 20*np.log10(np.maximum(np.abs(h), 1e-20))
 plt.plot(np.log10(w), db)
-#plt.ylim(-300, -0)
+plt.ylim(-80, +80)
 plt.xlim(-2, 3)
 plt.grid(True)
 #plt.yticks([0, -20, -40, -60, -80, -100, -120, -140, -160, -180])
@@ -34,8 +36,9 @@ plt.subplot(2, 1, 2)
 plt.plot(np.log10(w), np.angle(h))
 plt.grid(True)
 plt.xlim(-2, 3)
-plt.yticks([-np.pi, -0.5*np.pi, 0, 0.5*np.pi, np.pi],
-           [r'$-\pi$', r'$-\pi/2$', '0', r'$\pi/2$', r'$\pi$'])
+plt.ylim(-2, 0)
+#plt.yticks([-np.pi, -0.5*np.pi, 0],
+#           [r'$-\pi$', r'$-\pi/2$', '0'])
 #plt.xticks([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
 plt.ylabel('Phase [rad]')
 plt.xlabel('frequency [log10(Hz)]')
