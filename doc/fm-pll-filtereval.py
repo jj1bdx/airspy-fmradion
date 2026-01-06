95dd563f08d984b6660dc6baeb889cf233db22f0
Kenji Rikitake <kenji.rikitake@acm.org>
2026-01-06 10:23:29 +0900

    fm-pll-filtereval.py: add more comments

--- doc/fm-pll-filtereval.py
+++ doc/fm-pll-filtereval.py
@@ -5,18 +5,27 @@ import matplotlib.pyplot as plt
 
 # pure integrator with fixed gain of 80dB (10000)
 sosinteg = [10000.0, 0.0, 0.0, 1.0, -1.0, 0.0]
-# 2nd-order LPF
+# original 2nd-order LPF
 sosprelpf = [1.46974784e-06, 0, 0, 1, -1.99682419, 0.996825659]
-# 1st-order HPF (or inversed LPF)
+# original 1st-order HPF (or inversed LPF)
 sosfirsthpf = [0.000304341788, -0.000304324564, 0, 1, 0, 0]
+# approximated 2nd-order LPF by Butterworth design
+sosprelpf2 = signal.butter(2, 30, 'lowpass', False, 'sos', 384000)
 
+# reference open-loop charasteristics
 sosref = [sosprelpf, sosfirsthpf, sosinteg]
 
+# HPF only
 soshpfonly = [sosfirsthpf]
 
+# LPF only
 soslpfonly = [sosprelpf]
 
+# original twofilt
 sostwofilt = [sosprelpf, sosfirsthpf]
+ 
+# new twofilt
+sostwofilt = [sosprelpf2, sosfirsthpf]
 
 w, h = signal.sosfreqz(sosref, 38400000, False, 384000)
 
